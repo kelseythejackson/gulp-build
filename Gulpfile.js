e291f1538d682bad7c7860129259148ef08cf766
Kelsey Jackson <kelseythej@gmail.com>
2018-01-06 16:59:40 -0600

    Finish Exceeds

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,14 +3,15 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
-const cleanCss = require('gulp-clean-css');
+const csso = require('gulp-csso');
 const maps = require('gulp-sourcemaps');
 const imgMin = require('gulp-imagemin');
 const useref = require('gulp-useref');
-const gulpIf = require('gulp-if');
+const run = require('run-sequence');
 const replace = require('gulp-replace');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+
 
 gulp.task('scripts', ()=> {
     return gulp.src(['./js/jquery-3.2.1.min.js','./js/circle/autogrow.js', './js/circle/circle.js', './js/global.js'])
@@ -22,16 +23,23 @@ gulp.task('scripts', ()=> {
         .pipe(gulp.dest('./dist/scripts'));
 });
 
-gulp.task('styles', () => {
-   return gulp.src('./sass/**/*.scss')
+gulp.task('sass', () => {
+    return gulp.src('./sass/**/*.scss')
        .pipe(maps.init())
        .pipe(sass())
+       .pipe(maps.write('.'))
        .pipe(gulp.dest('./css'))
-       .pipe(cleanCss())
+});
+
+gulp.task('styles', ['sass'], () => {
+   return gulp.src('./css/global.css')
+       .pipe(maps.init())
        .pipe(rename('all.min.css'))
+       .pipe(csso())
        .pipe(maps.write('.'))
        .pipe(gulp.dest('./dist/styles'))
        .pipe(browserSync.stream());
+
 });
 
 gulp.task('images', () => {
@@ -40,7 +48,12 @@ gulp.task('images', () => {
         .pipe(gulp.dest('./dist/content'))
 });
 
-gulp.task('html', ['scripts', 'styles', 'images'], () => {
+gulp.task('icons', () => {
+     return gulp.src(['./icons/**/**/*'])
+        .pipe(gulp.dest('./dist/icons'));
+});
+
+gulp.task('html',['icons'], () => {
     return gulp.src('./index.html')
     .pipe(useref())
     .pipe(replace('images/', 'content/'))
@@ -49,7 +62,7 @@ gulp.task('html', ['scripts', 'styles', 'images'], () => {
 
 gulp.task('serve', () => {
     browserSync.init({
-        server: '.'
+        server: './dist'
     });
 
     gulp.watch('./sass/**/*.scss', ['styles']);
@@ -60,12 +73,11 @@ gulp.task('clean', ()=> {
     del(['dist']);
 });
 
- gulp.task('build', ['html'], () => {
-     return gulp.src(['./icons/**/**/*'])
-        .pipe(gulp.dest('./dist/icons'));
+ gulp.task('build', ['clean'], (callback) => {
+     run('sass','icons', 'html', 'scripts', 'images', 'styles', callback);
  });
 
- gulp.task('default', ['clean'], () => {
-    gulp.start('build');
+ gulp.task('default', ['build'], () => {
     gulp.start('serve');
  });
+
